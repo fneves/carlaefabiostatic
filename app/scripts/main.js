6835b0b1378ce399cef04fa333c1ff7ab81bda93
Fabio Neves <fneves@zendesk.com>
2016-02-06 14:06:07 +0000

    Updating styles

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,20 @@
 console.log('\'Allo \'Allo!'); // eslint-disable-line no-console
+
+$(".anchor").on('click', function(event){
+
+    // Prevent default anchor click behavior
+    event.preventDefault();
+
+    // Store hash (#)
+    var hash = this.hash;
+
+    // Using jQuery's animate() method to add smooth page scroll
+    // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area (the speed of the animation)
+    $('html, body').animate({
+        scrollTop: $(hash).offset().top
+    }, 800, function(){
+
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+    });
+});
